ed206dbe314a21cbd27c1259ef16c0fdba382447
neelshinde-git <66645545+neelshinde-git@users.noreply.github.com>
2020-12-01 02:07:18 +0530

    Delete store.js

--- frontend_submission/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from "redux-thunk"
-import reducer from './reducer';
-
-
-const store = createStore(reducer, applyMiddleware(ReduxThunk));
-
-
-export default store;
